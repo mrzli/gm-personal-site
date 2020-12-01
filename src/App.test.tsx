dbaf3617a792ab3a1d090cc01e4930e9e813b9fd
Goran Mržljak <goran.mrzljak@gmail.com>
2020-12-01 17:19:31 +0100

    Updates

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,4 +5,7 @@ test('renders learn react link', () => {
   render(<App />);
   const textElement = screen.getByText(/Hello React/i);
   expect(textElement).toBeInTheDocument();
+  const actual = 1;
+  const expected = 1;
+  expect(actual).toEqual(expected);
 });
