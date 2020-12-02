66479f5d3cb9b91b1cb7015de53848442b84a323
Goran Mržljak <goran.mrzljak@gmail.com>
2020-12-02 11:59:04 +0100

    Some fiddling with tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
-test('renders learn react link', () => {
+it('renders hello react', () => {
   render(<App />);
   const textElement = screen.getByText(/Hello React/i);
   expect(textElement).toBeInTheDocument();
-  const actual = 1;
-  const expected = 1;
-  expect(actual).toEqual(expected);
 });
