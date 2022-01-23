3ce054fb33b193680c2fba5f40fc6e021fc70679
Goran Mržljak <goran.mrzljak@gmail.com>
2022-01-23 21:45:57 +0100

    update react router

--- src/components/base/ScreenRoutes.tsx
+++ src/components/base/ScreenRoutes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { IntroScreen } from '../screens/IntroScreen';
 import { WorkHistoryScreen } from '../screens/WorkHistoryScreen';
 import { PersonalInfoScreen } from '../screens/PersonalInfoScreen';
@@ -8,35 +8,17 @@ import { SkillsOverviewScreen } from '../screens/SkillsOverviewScreen';
 import { ProsAndConsScreen } from '../screens/ProsAndConsScreen';
 import { PersonalProjectsScreen } from '../screens/PersonalProjectsScreen';
 
-interface ScreenRoutesProps {}
-
-export function ScreenRoutes(props: ScreenRoutesProps): React.ReactElement {
+export function ScreenRoutes(): React.ReactElement {
   return (
-    <Switch>
-      <Route exact={true} path={'/'}>
-        <IntroScreen />
-      </Route>
-      <Route path={'/work-history'}>
-        <WorkHistoryScreen />
-      </Route>
-      <Route path={'/education'}>
-        <EducationScreen />
-      </Route>
-      <Route path={'/skills-overview'}>
-        <SkillsOverviewScreen />
-      </Route>
-      <Route path={'/pros-and-cons'}>
-        <ProsAndConsScreen />
-      </Route>
-      <Route path={'/personal-projects'}>
-        <PersonalProjectsScreen />
-      </Route>
-      <Route path={'/personal-info'}>
-        <PersonalInfoScreen />
-      </Route>
-      <Route path={'*'}>
-        <Redirect to={'/'} />
-      </Route>
-    </Switch>
+    <Routes>
+      <Route path={'/'} element={<IntroScreen />} />
+      <Route path={'/work-history'} element={<WorkHistoryScreen />} />
+      <Route path={'/education'} element={<EducationScreen />} />
+      <Route path={'/skills-overview'} element={<SkillsOverviewScreen />} />
+      <Route path={'/pros-and-cons'} element={<ProsAndConsScreen />} />
+      <Route path={'/personal-projects'} element={<PersonalProjectsScreen />} />
+      <Route path={'/personal-info'} element={<PersonalInfoScreen />} />
+      <Route path={'*'} element={<IntroScreen />} />
+    </Routes>
   );
 }
